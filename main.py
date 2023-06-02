22775637cc4006254c1c7e0e80099dafc34479a0
Jacob Hill <hilljake93@gmail.com>
2023-06-02 11:01:13 -0400

    nada

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 # Define the repository path
 repository_path = '/Users/jacobhill/Desktop/new_line'
@@ -8,14 +9,14 @@ file_path = '/Users/jacobhill/Desktop/new_line/print.py'
 new_line = 'print("Hello, There!")'
 
 # Change directory to the repository path
-subprocess.call(['cd', repository_path])
+subprocess.call(['cd', repository_path], stdout=open(os.devnull, "wb"))
 
 # Add the new line of code to the file
-subprocess.call(['echo', new_line, '>>', file_path])
+subprocess.call(['echo', new_line, '>>', file_path], stdout=open(os.devnull, "wb"))
 
 # Commit the changes
-subprocess.call(['git', 'add', file_path])
-subprocess.call(['git', 'commit', '-m', 'Added new line of code'])
+subprocess.call(['git', 'add', file_path], stdout=open(os.devnull, "wb"))
+subprocess.call(['git', 'commit', '-m', 'Added new line of code'], stdout=open(os.devnull, "wb"))
 
 # Push the changes to GitHub
-subprocess.call(['git', 'push', 'origin', 'main'])
+subprocess.call(['git', 'push', 'origin', 'main'], stdout=open(os.devnull, "wb"))
